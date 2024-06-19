0e7a6cd2063246543d8aa2d12fef379f796b56db
Nghia Le <nghiapro2611@gmail.com>
2024-06-19 17:35:46 +0700

    Commit

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,35 @@
+// @ts-nocheck
 /* eslint-disable no-undef */
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
+// import { nodePolyfills } from 'vite-plugin-node-polyfills';
+
 // https://vitejs.dev/config/
-export default defineConfig({
-	resolve: {
-		alias: {
-			'@': path.resolve(__dirname, './src'),
-			// '@assets': path.resolve(__dirname, './src/assets'),
-			// '@components': path.resolve(__dirname, './src/components'),
+export default defineConfig(() => {
+	// const env = dotenv.config({path: `.env-cmdrc.json`}).parsed;
+	// expand(env);
+
+	return {
+		define: {
+			'process.env': {},
+			global: {},
 		},
-	},
-	plugins: [react()],
+		plugins: [react()],
+		resolve: {
+			alias: {
+				'@': path.resolve(__dirname, './src'),
+				buffer: 'buffer/',
+				util: 'util/',
+				process: 'process/browser',
+				stream: 'stream-browserify',
+				crypto: 'crypto-browserify',
+			},
+		},
+		optimizeDeps: {
+			esbuildOptions: {
+				jsx: 'automatic',
+			},
+		},
+	};
 });
